050e4ba54cf9b42eb036f96ef3d0f703650cf808
subeda2023 <147141868+subeda2023@users.noreply.github.com>
2023-12-12 13:44:46 +0100

    Create ListerUtilisateur.java
    ajout servlet lister

---
+++ AppliEnchere/src/main/java/fr/eni/Appli/Enchere/ihm/ListerUtilisateur.java
@@ -0,0 +1,63 @@
+package fr.eni.Appli.Enchere.ihm;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.Aplli.Enchere.bll.bo.Utilisateur;
+import fr.eni.Appli.Enchere.bll.UtilisateurManager;
+
+
+public class ListerUtilisateur extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		{
+			HttpSession sess;
+			sess =request.getSession();
+			Utilisateur utilisateur;
+			utilisateur=(Utilisateur)sess.getAttribute("userConnected");
+			if(utilisateur!=null)
+			{
+				List<Utilisateur> lst = null;
+				
+					try {
+						lst=UtilisateurManager.getInstance().findAll();
+					     } 
+					catch (ClassNotFoundException e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+				    } 
+					catch (SQLException e) 
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				
+				request.setAttribute("liste", lst);
+				getServletContext().getRequestDispatcher("/WEB-INF/listeUtilisateur.jsp").forward(request, response);
+			}
+			else
+			{
+			}
+			
+		}
+		
+	}
+	
+
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
