36ec4727cf2e3275eff10d3b30206525e49d07ec
subeda2023 <147141868+subeda2023@users.noreply.github.com>
2023-12-12 10:20:37 +0100

    Update ModifierUtilisateur.java
    modif Utilisateur mise à jour 12.12

--- AppliEnchere/src/main/java/fr/eni/Appli/Enchere/ihm/ModifierUtilisateur.java
+++ AppliEnchere/src/main/java/fr/eni/Appli/Enchere/ihm/ModifierUtilisateur.java
@@ -22,6 +22,7 @@ public class ModifierUtilisateur extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		int noUtilisateur = Integer.parseInt(request.getParameter("no_utilisateur"));
 		try {
+			//recherche de l'utilisateur à modifier
 			request.setAttribute("utilisateur", UtilisateurManager.getInstance().findById(noUtilisateur));
 			getServletContext().getRequestDispatcher("/WEB-INF/modifier.jsp").forward(request, response);
 		} catch (ClassNotFoundException e) {
@@ -38,7 +39,10 @@ public class ModifierUtilisateur extends HttpServlet {
 		int noUtilisateur = Integer.parseInt(request.getParameter("no_utilisateur"));
 		//String email = request.getParameter("email");
 		try {
+			//rechercher de l'utilisateur à modifier
 			utilisateur = UtilisateurManager.getInstance().findById(noUtilisateur);
+			
+			//modification des champs nécessaires
 			utilisateur.setPseudo(request.getParameter("pseudo"));
 			utilisateur.setNom(request.getParameter("nom"));
 			utilisateur.setPrenom(request.getParameter("prenom"));	
@@ -48,11 +52,13 @@ public class ModifierUtilisateur extends HttpServlet {
 			utilisateur.setCodePostal(request.getParameter("code_postal"));
 			utilisateur.setVille(request.getParameter("ville"));
 			utilisateur.setMotDePasse(request.getParameter("mot_de_passe"));
+			
+			//mettre à jour l'utilisateur
 			UtilisateurManager.getInstance().update(utilisateur);
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		} 
-			
+			// renvoyer vers la liste des utilisateurs
 			response.sendRedirect("lister");
 				
 				
